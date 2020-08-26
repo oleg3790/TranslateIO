96b8bde86842aa954d9922e552008f33226795ed
oleg3790 <oleg.krysko@outlook.com>
2020-08-25 23:47:26 -0500

    Highlighted text translation

--- src/extension.ts
+++ src/extension.ts
@@ -1,27 +1,38 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import translate = require('google-translate-api');
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "translateio" is now active!');
+	let translateCommand = vscode.commands.registerCommand('translateio.translate', async () => {
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('translateio.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
+		const currentEditor = vscode.window.activeTextEditor;
+		const currentSelection = currentEditor?.selection;
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from TranslateIO!');
+		const start = currentSelection?.start;
+		const end = currentSelection?.end;
+
+		// If there is a selection range, parse
+		if (start && end && start.character !== end.character) {
+			const selectionRange = new vscode.Range(start, end);
+			const highlightedText = currentEditor?.document.getText(selectionRange);
+
+			if (highlightedText) {
+				const config = vscode.workspace.getConfiguration('translateio');
+
+				try {
+					const result = await translate(highlightedText, { from: config.get<string>('fromLanguage'), to: config.get<string>('toLanguage') });
+					vscode.window.showInformationMessage(`Translated From: ${highlightedText} - To: ${result.text}`);
+					return;
+				} catch (ex) {
+					console.error(ex);
+				}
+			}
+		}
+
+		vscode.window.showInformationMessage('Highlight a range to use in the translation, TranslateIO will parse the strings in this range');
 	});
 
-	context.subscriptions.push(disposable);
+	context.subscriptions.push(translateCommand);
 }
 
-// this method is called when your extension is deactivated
 export function deactivate() {}
