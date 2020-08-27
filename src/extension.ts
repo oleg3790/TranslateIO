143ef648620e0cc219a1bcdc016f9214fb2f8a01
oleg3790 <oleg.krysko@outlook.com>
2020-08-26 21:44:49 -0500

    Organize logic

--- src/extension.ts
+++ src/extension.ts
@@ -1,36 +1,50 @@
 import * as vscode from 'vscode';
 const translate = require('@vitalets/google-translate-api');
 
+const getHighlightedText = (): string | undefined => {
+	const currentEditor = vscode.window.activeTextEditor;
+	const currentSelection = currentEditor?.selection;
+
+	const start = currentSelection?.start;
+	const end = currentSelection?.end;
+
+	// If there is a selection range, parse
+	if (start && end && start.character !== end.character) {
+		const selectionRange = new vscode.Range(start, end);
+		const highlightedText = currentEditor?.document.getText(selectionRange);
+
+		if (highlightedText) {
+			return highlightedText;
+		}
+	}
+
+	vscode.window.showInformationMessage('Highlight some text to translate');
+};
+
+const getTranslationConfig = (): { from: string | undefined, to: string | undefined }  => {
+	const config = vscode.workspace.getConfiguration('translateio');
+	return {
+		to: config.get<string>('toLanguage'),
+		from: config.get<string>('fromLanguage')
+	};
+};
+
+const translateContent = async () => {
+	try {
+		const highlightedText = getHighlightedText();
+		const config = getTranslationConfig();
+
+		const result = await translate(highlightedText, { from: config.from, to: config.to });
+
+		vscode.window.showInformationMessage(`Translated Successfully!`);
+	} catch (ex) {
+		vscode.window.showErrorMessage('Error encountered while attempting to translate content');
+	}
+};
+
 export function activate(context: vscode.ExtensionContext) {
 
-	let translateCommand = vscode.commands.registerCommand('translateio.translate', async () => {
-
-		const currentEditor = vscode.window.activeTextEditor;
-		const currentSelection = currentEditor?.selection;
-
-		const start = currentSelection?.start;
-		const end = currentSelection?.end;
-
-		// If there is a selection range, parse
-		if (start && end && start.character !== end.character) {
-			const selectionRange = new vscode.Range(start, end);
-			const highlightedText = currentEditor?.document.getText(selectionRange);
-
-			if (highlightedText) {
-				const config = vscode.workspace.getConfiguration('translateio');
-
-				try {
-					const result = await translate(highlightedText, { from: config.get<string>('fromLanguage'), to: config.get<string>('toLanguage') });
-					vscode.window.showInformationMessage(`Translated From: ${highlightedText} - To: ${result.text}`);
-					return;
-				} catch (ex) {
-					console.error(ex);
-				}
-			}
-		}
-
-		vscode.window.showInformationMessage('Highlight a range to use in the translation, TranslateIO will parse the strings in this range');
-	});
+	let translateCommand = vscode.commands.registerCommand('translateio.translate', async () => translateContent());
 
 	context.subscriptions.push(translateCommand);
 }
